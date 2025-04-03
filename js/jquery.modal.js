897f350fc43c9168d3cd206b262d46639d39356b
caleb <brick.cuppy@gmail.com>
2025-04-02 18:54:28 -0700

    well, I can't figure out how to make the slideshow work :(

--- js/jquery.modal.js
+++ js/jquery.modal.js
@@ -1,13 +1,13 @@
 function addContent(img) {
 	// Set the image source/alt to whichever image the user clicked on
-	$("#modal > img").attr("src", img.attr("src"));
-	$("#modal > img").attr("alt", img.attr("alt"));
+	$("#modal-img").attr("src", img.attr("src"));
+	$("#modal-img").attr("alt", img.attr("alt"));
 
 	// Change caption so that it lists the team who made the art in the modal
 	$("#caption").text(function (i, originalText) {
-		return "By Team " + img.attr("alt");
+		return "By " + img.attr("alt");
 	});
-};
+}
 
 $(document).ready(function () {
 	// Open modal and add content when user clicks on an image
@@ -17,7 +17,7 @@ $(document).ready(function () {
 	});
 
 	// Close modal when user clicks the X icon or the image inside the modal
-	$("#close, #modal > img").click(function () {
+	$("#close, #modal-img").click(function () {
 		$("#modal").toggle();
 	});
 });
