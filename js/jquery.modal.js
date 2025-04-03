ceb9cc36023cd2d56d8c00dbef4d6d28ecb15f6b
caleb <brick.cuppy@gmail.com>
2025-04-02 17:26:10 -0700

    Small changes, attempting to turn the modal into a slideshow

--- js/jquery.modal.js
+++ js/jquery.modal.js
@@ -1,24 +1,23 @@
+function addContent(img) {
+	// Set the image source/alt to whichever image the user clicked on
+	$("#modal > img").attr("src", img.attr("src"));
+	$("#modal > img").attr("alt", img.attr("alt"));
+
+	// Change caption so that it lists the team who made the art in the modal
+	$("#caption").text(function (i, originalText) {
+		return "By Team " + img.attr("alt");
+	});
+};
+
 $(document).ready(function () {
 	// Open modal and add content when user clicks on an image
 	$("img.open-in-modal").click(function () {
-		let img = $(this);
-		
-		// Set the image source/alt to whichever image the user clicked on
-		$("#modal > img").attr("src", img.attr("src"));
-		$("#modal > img").attr("alt", img.attr("alt"));
-
-		// Change caption so that it lists the team who made the art in the modal
-		$("#caption").text(function (i, originalText) {
-			return "By Team " + img.attr("alt");
-		}); 
+		addContent($(this));
 		$("#modal").toggle();
 	});
 
 	// Close modal when user clicks the X icon or the image inside the modal
-	$("#close").click(function () {
+	$("#close, #modal > img").click(function () {
 		$("#modal").toggle();
 	});
-	$("#modal > img").click(function () {
-		$("#modal").toggle();
-	})
 });
